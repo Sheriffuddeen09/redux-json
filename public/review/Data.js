090dcaa954f7f4209641f85512f8c25435fbf43b
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-12-19 12:27:51 +0100

    commit

---
+++ public/review/Data.js
@@ -0,0 +1,413 @@
+import slidera from './image/satelites.webp'
+import sliderb from './image/spacelinkb.jpg'
+import sliderc from './image/spacerocket.jpg'
+import sliderd from './image/flight.webp'
+import slidere from './image/accelerate.jpg'
+import sliderf from './image/rider.jpg'
+import sliderg from './image/shipment.jpg'
+import sliderh from './image/mover.webp'
+import themes from './themes/theme1.avif'
+import logistic from './image/fundament.webp'
+import logisticb from './image/supply.jpg'
+import logisticc from './image/onlinetech.jpg'
+import logisticc from './image/logic.jpg'
+import logisticc from './image/tech.webp'
+import logisticc from './image/feedback.jpg'
+import customer from './image/customer.jpg'
+import customerb from './image/rate.png'
+import customerc from './image/security.png'
+import customerd from './image/compliance.png'
+import customere from './image/high.png'
+import customerf from './image/flexibility.webp'
+import companya from './image/dangote.png'
+import companyb from './image/global.png'
+import companyc from './image/tiny.png'
+import companyd from './image/micro.png'
+import companye from './image/afex.png'
+
+export const homesdata = [
+    {
+        id:1,
+        images:slidera,
+        title:"RECENT WATCH",
+        body:"STARLINK MISSION",
+        click:"REWATCH" 
+    },
+    {
+        id:2,
+        images:sliderb,
+        title:"RECENT LAUNCH",
+        body:"GALILEO L13 MISSION",
+       click: "REWATCH"
+    },
+    {
+        id:3,
+        images:sliderc,
+        title:"RECENT MISSION",
+        body:"POLARIS DAWN",
+       click: "REWATCH"
+    },
+    {
+        images:sliderd,
+        title:"RECENT WATCH",
+        body:"ADVANCING HUMAN SPACEFLIGHT",
+       click: "LEARN MORE"
+    },
+    {
+        images:slidere,
+        title:"",
+        body:"TO MAKE LIFE MULTIPLANETARY",
+       click: "LEARN MORE"
+    }
+]
+export const rider = [
+    {
+        id:1,
+        images:sliderf,
+        title:"Logistics Rider"
+    },
+    {
+        id:2,
+        images:sliderg,
+        title:"Shipment"
+    },
+    {
+        id:3,
+        images:sliderh,
+        title:"Bulk Deliveries"
+    }
+]
+export const welcomes=[
+    
+    {
+        images:themes,
+        welcome:"Welcome to Ecoreme ",
+        title:"Choose your path to success",
+        company:"Ecoreme",
+        main:"Logistics",
+        tech:"Technologies"
+       
+    }
+]
+
+    export const startup = [
+        {
+            id:1,
+            body:"Reliable Logistics at Your Doorstep! Need fast, secure, and hassle-free PICKUP or DELIVERY in Lagos?.",
+            main:"Ecoreme Logistics offers seamless service for all your shipping needs! Whether it's a single package pr bulk orders, we ensure timely and efficient service at competitive rates.",
+            trust:" Trust us to handle your deliveries with care, every time"
+        }
+    ]
+    export const logistics =[
+        {
+            id:1,
+            image:logistic,
+            logistic:Logistic,
+            type:"Transport",
+            type2:"Acquired",
+            type3:"Stored",
+            quality:"Time move",
+            quality2:"Time arrive",
+            quality3:"Online"
+        },
+        {
+            id:1,
+            image:logisticb,
+            logistic:'Logistic',
+            type:"Transport",
+            type2:"Acquired",
+            type3:"Stored",
+            quality:"Time move",
+            quality2:"Time arrive",
+            quality3:"Online"
+        },
+        {
+            id:1,
+            logistic:'Logistic',
+            image:logisticc,
+            type:"Transport",
+            type2:"Acquired",
+            type3:"Stored",
+            quality:"Time move",
+            quality2:"Time arrive",
+            quality3:"Online"
+        }
+
+    ]
+    export const technologys = [
+        {
+            id:1,
+            image:tech,
+            logistic:"Technologies",
+            type:"Knowledge",
+            type2:"Reproducible",
+            type3:"Phrased",
+            quality:"Set",
+            quality2:"Goal",
+            quality3:"Achieve"
+        },
+        {
+            id:1,
+            image:techb,
+            logistic:"Technologies",
+            type:"Technologies",
+            type2:"Reproducible",
+            type3:"Phrased",
+            quality:"Set",
+            quality2:"Goal",
+            quality3:"Achieve"
+        },
+        {
+            id:1,
+            logistic:"Technologies",
+            image:techc,
+            type:"Technologies",
+            type2:"Reproducible",
+            type3:"Phrased",
+            quality:"Set",
+            quality2:"Goal",
+            quality3:"Achieve"
+        }
+
+    ]
+    
+    export const onelogistics = [
+        {
+            id:1,
+            image:logistic,
+            logistic:Logistic,
+            type:"Transport",
+            type2:"Acquired",
+            type3:"Stored",
+            quality:"Time move",
+            quality2:"Time arrive",
+            quality3:"Online"
+        }
+    ]
+    export const twologistics = [
+        {
+            id:1,
+            image:logistic,
+            logistic:Logistic,
+            type:"Transport",
+            type2:"Acquired",
+            type3:"Stored",
+            quality:"Time move",
+            quality2:"Time arrive",
+            quality3:"Online"
+        },
+        {
+            id:1,
+            logistic:Logistic,
+            image:logisticb,
+            type:"Transport",
+            type2:"Acquired",
+            type3:"Stored",
+            quality:"Time move",
+            quality2:"Time arrive",
+            quality3:"Online"
+        }
+    ]
+    export const onetechs = [
+        {
+            id:1,
+            image:tech,
+            logistic:"Technologies",
+            type:"Technologies",
+            type2:"Reproducible",
+            type3:"Phrased",
+            quality:"Set",
+            quality2:"Goal",
+            quality3:"Achieve"
+        }
+    ]
+    export const twotechs = [
+        {
+            id:1,
+            image:tech,
+            logistic:"Technologies",
+            type:"Technologies",
+            type2:"Reproducible",
+            type3:"Phrased",
+            quality:"Set",
+            quality2:"Goal",
+            quality3:"Achieve"
+        },
+        {
+            id:1,
+            logistic:"Technologies",
+            image:techb,
+            type:"Technologies",
+            type2:"Reproducible",
+            type3:"Phrased",
+            quality:"Set",
+            quality2:"Goal",
+            quality3:"Achieve"
+        }
+    ]
+    export const logs =[
+        {
+            dedicate:"We are Dedicated and dead serious About our business",
+            view:"Want to view our exclusive special rates?",
+            sign:"Sign in or create an account to view all",
+            liner:"our available special rates for this liner.",
+            service:"Ecoreme Provide Awesome complex operation, refers to the overall process of managing how your resources are Acquired, stored and transported to their final destination. And use own our patented system called Logistic, short for Client, Culture and Company. Our specialists are able to 'infiltrate' all company sections and analyze for areas where improvement is needed "
+        }
+    ]
+    export const solutions = [
+        {
+            image:customer,
+            title:"Customer Service Excellence",
+            body:"We prioritize personalized support to ensure a smooth and satisfying experience for every client. Our dedicated team is alwaysready to address concerns and provide timely solutions.",
+            "icon":customerb,
+            "numbertitle":"Affordable Rates",
+            "numberBody":"We offer competitive pricing without compromising the quality of service. Our cost-effective solutions help busimesses manage logistics within budget."
+        },
+        {
+            image:customerc,
+            title:"Security",
+            body:"Your shipments are safeguarded with advanced tracking and security measures. We ensure that your cargo reaches its destination intact and on time.",
+            "icon":customerd,
+            "numbertitle":"Compliance",
+            "numberBody":"Our operations adhere to all local and international regulations. We ensure ful compliance with industry standards and customs requirements."
+        },
+        {
+            image:customere,
+            title:"Highly Available",
+            body:"Our services are operations 24/7 to meet your logistics needs anytime. We guarantee consistent availability, no matter the time or location.",
+            "icon":customerf,
+            "numbertitle":"Flexibility",
+            "numberBody":"We adapt to your specific requirements, offering tailored solutions for various logistics challenges. Our flexible approach ensures seamless operations in changing conditions."
+        }
+    ]
+    export const companys = [
+        {
+            id:1,
+            image:companya
+        },
+        {
+            id:2,
+            image:companyb
+        },
+        {
+            id:3,
+            image:companyc
+        },
+        {
+            id:1,
+            image:companyd
+        },
+        {
+            id:1,
+            image:companye
+        }
+    ],
+    "faqs": [
+        {
+            id: 1,
+            title: "What do you do at Ecoreme",
+            body: "We provide digital freight management services to Organization's cross-border traders. Our web-based application enables you search negotiated real-time rates and comprehensive quotes. Our clients are also able to track and monitor the progress of their Stored Resources from transport to delivery, from anywhere in the world. We offer logistics and technology, pay as you go and off-dock terminal handling services, and marine insurance."
+        },
+    {
+        id: 2,
+        title: "Create an account",
+        body: "Click on Create Account to initiate the Logistic and Technologies process."
+    },
+    {
+        id: 3,
+        title: "How Your Resources are Store",
+        body: " Log in to Ecoreme to gain access to our streamlined Logistic services, competitive rates, and outstanding customer support."
+    },
+    {
+        id: 4,
+        title: "Confirm your Stored / Acquired Resource.",
+        body: "Our exceptional customer support team will promptly follow up with you and provide updates on all your Resources."
+    },
+    {
+        id: 5,
+        title: "Track and Monitor Resources",
+        body: "Keep track of your Stored/Acquired in transit with our reliable tracker, allowing you to stay informed about their whereabouts."
+    }
+,
+    {
+        id: 6,
+        title: "How can I track my Resourses?",
+        body: "A tracking link will be assigned to your Stored/Acquired Resources once your booking has been confirmed. With this, you are able to track the status of your order from your dashboard."
+    }
+],
+"reviews":[
+    {
+        id:1,
+        "star":"review/stars.png",
+        image:"review/backend.jpg",
+        "user":"Smith David",
+        body:"Very Good service all trust them on their delivery and other service. They are Dedicate to timing in delivery to their customer. I suggest everyone to choose this courier service",
+        "icon":"review/face.png"
+    },
+    {
+        id:2,
+        "star":"review/stars.png",
+        image:"review/gg.jpg",
+        "user":"Paul Olive",
+        body:"Very Good service ever",
+        "icon":"review/face.png"
+    },
+    {
+        id:3,
+        image:"review/python.png",
+        "star":"review/stars.png",
+        "user":"Abdul-Salam Ola",
+        body:"I selected this vendor based on their google rating 4.5+. But honestly speaking their service deserved more than that rating.",
+        "icon":"review/face.png"
+    },
+    {
+        id:4,
+        image:"review/backend.jpg",
+        "star":"review/stars.png",
+        "user":"Smith David",
+        body:"The main thing I like best about supply chain management is that it is a broad discipline and touches on many parts of a company",
+        "icon":"review/face.png"
+    },
+    {
+        id:5,
+        image:"review/unlimited.png",
+        "star":"review/stars.png",
+        "user":"Fawas Olatunji",
+        body:"Ecoreme was competent in doing anything. They just stand on their trust",
+        "icon":"review/face.png"
+    },
+    {
+        id:6,
+        image:"review/image-1.jpeg",
+        "star":"review/stars.png",
+        "user":"Mutmainat Ojulari",
+        body:"Ecoreme Logistics is best and professional moving company.",
+        "icon":"review/face.png"
+    }
+],
+"review":[
+    {
+        id:1,
+        image:"review/image-3.jpeg",
+        "star":"review/stars.png",
+        "user":"Chika Ugo",
+        body:"There is no doubt that logistics plays a vital role in our economy and its importance is only increasing as businesses become more globalized. Without efficient logistics, it would be impossible for business to move products around the world a timely and cost-effective manner",
+        "icon":"review/face.png"
+    },
+    {
+        id:2,
+        "star":"review/stars.png",
+        image:"review/image-2.jpeg",
+        "user":"Elizabeth Gloria",
+        body:"I had used the service of Ecoreme and Movers 2 weeks back to parcel my 2 wheeler from Lagos to Benin. Quiet happy with their service",
+        "icon":"review/face.png"
+    },
+    {
+        id:3,
+        image:"review/web.jpg",
+        "star":"review/stars.png",
+        "user":"Peter Akon",
+        body:"Lovely service rendering",
+        "icon":"review/face.png"
+    }
+]
